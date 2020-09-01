2d13f19f6a2c9acc1889a213b1da97911c7da555
rohit sohlot <5600987+rsohlot@users.noreply.github.com>
2020-09-01 22:31:10 +0530

    solution
    cleared all the cases

---
+++ angry_professor.py
@@ -0,0 +1,39 @@
+# problem: https://www.hackerrank.com/challenges/angry-professor/problem
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the angryProfessor function below.
+def angryProfessor(k, a):
+    presents = 0
+    for e in a:
+        if e<=0:
+            presents+=1
+    if k<= presents:
+        return "NO"
+    else:
+        return "YES"
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input())
+
+    for t_itr in range(t):
+        nk = input().split()
+
+        n = int(nk[0])
+
+        k = int(nk[1])
+
+        a = list(map(int, input().rstrip().split()))
+
+        result = angryProfessor(k, a)
+
+        fptr.write(result + '\n')
+
+    fptr.close()
